27ca1727164414e5ac059a32cdd808e70c0ef425
jinhoyoho <jinho7018@naver.com>
2024-05-27 23:41:29 +0900

    Add: 지도 제작중

---
+++ taba/src/components/Map.js
@@ -0,0 +1,8 @@
+import React, { useState, useEffect } from "react";
+import "../styles/map.css";
+
+function Map() {
+  return <div className="custom-map">지도가 들어갈 곳 입니다.</div>;
+}
+
+export default Map;
