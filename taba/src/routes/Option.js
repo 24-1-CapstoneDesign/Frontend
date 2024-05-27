f53cb5da26159f2dd18f601330d6bf63e7a9dcc4
jebomin <jebomin0515@naver.com>
2024-05-27 11:03:50 +0900

    Design: USER MANAGEMENT 페이지 레이아웃 조정

--- taba/src/routes/Option.js
+++ taba/src/routes/Option.js
@@ -1,21 +1,23 @@
+import React from "react";
 import LeftNav from "../components/LeftNav";
 import UserTable from "../components/UserTable";
 import NavBar from "../components/NavBar";
 import OptionGraph from "../components/OptionGraph";
+import "../styles/option.css";
 
 export default function Option() {
   return (
     <div>
-      <div>
+      <div className="navbar">
         <NavBar />
       </div>
-      <div>
+      <div className="left-nav">
         <LeftNav />
       </div>
-      <div>
+      <div className="user-table">
         <UserTable />
       </div>
-      <div>
+      <div className="option-graph">
         <OptionGraph />
       </div>
     </div>
