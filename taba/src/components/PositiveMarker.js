ee5643d7864560cb415ef941ec7ac4721e76ded1
jinhoyoho <jinho7018@naver.com>
2024-05-29 22:46:55 +0900

    Add: 메인화면 제작 완료

--- taba/src/components/PositiveMarker.js
+++ taba/src/components/PositiveMarker.js
@@ -1,12 +1,12 @@
 import React from "react";
 import "../styles/positivemarker.css";
 
-const PositiveMarker = () => {
+const PositiveMarker = (props) => {
   return (
     <div className="PM-container">
       <div className="PM-top-section"></div>
       <div className="PM-bottom-section"></div>
-      <p className="PM-id-text">ID : 5</p>
+      <p className="PM-id-text">ID : {props.id}</p>
       <p className="PM-status-text">해결</p>
     </div>
   );
