1b4774fe1a810498d97512ce809faddeb8ddf0f8
jebomin <jebomin0515@naver.com>
2024-06-11 03:44:29 +0900

    Feat: 사용자 관리 페이지 완료(위도 경도 주소로 변경 필요)

--- src/services/userTable.js
+++ src/services/userTable.js
@@ -1,4 +1,3 @@
-// 실제 환경에서는 이 URL을 실제 서버의 URL로 바꿔야 함
 const baseUrl = "http://localhost:3000/api";
 
 // 모든 사용자 정보를 가져오는 함수
@@ -15,6 +14,28 @@ export const fetchCarsByUserId = async (userId) => {
   const response = await fetch(`${baseUrl}/cars/user/${userId}`);
   if (!response.ok) {
     throw new Error(`Failed to fetch cars for user ${userId}`);
+  }
+  return await response.json();
+};
+
+// 특정 사용자 ID에 대한 모든 운전 세션 정보를 가져오는 함수
+export const fetchDrivingSessionsByUser = async (userId) => {
+  const response = await fetch(
+    `${baseUrl}/drivingsessions/findbyuser/${userId}`
+  );
+  if (!response.ok) {
+    throw new Error(`Failed to fetch driving sessions for user ${userId}`);
+  }
+  return await response.json();
+};
+
+// 특정 차량 번호에 대한 모든 운전 세션 정보를 가져오는 함수
+export const fetchDrivingSessionsByCarNumber = async (carNumber) => {
+  const response = await fetch(
+    `${baseUrl}/drivingsessions/findbycar/${carNumber}`
+  );
+  if (!response.ok) {
+    throw new Error(`Failed to fetch driving sessions for car ${carNumber}`);
   }
   return await response.json();
 };
